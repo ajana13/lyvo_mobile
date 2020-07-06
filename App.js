79670593ffccf9a19b446252c3a48f1e78e08370
Anushree Jana <anushreejana@1x-nat-vl932-172-30-220-214.wireless.umass.edu>
2020-07-05 20:24:40 -0400

    App Start

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Constants } from 'expo';
+import { TextInput } from 'react-native-gesture-handler';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React {
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: ''
+    }
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <StatusBar style="auto" /> */}
+        <View>
+          <TextInput placeholder='Enter your message'
+            onChangeText={(text) => this.setState({ message: text })} />
+          <Button title='Send' onPress={this.addItem} />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#eee',
+    marginTop: Constants.statusBarHeight,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
